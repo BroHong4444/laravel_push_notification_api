b28304c273239173908e50aed1982be6781f3af2
Heng Hong <hongofficial4444@gmail.com>
2025-08-07 17:01:25 +0700

    log

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -11,10 +12,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('/send-report', [ReportController::class, 'sendToTelegram']);
 
 Route::get('/hello-laravel', function () {
+    Log::info('🔔 POST /hello-laravel hit');
+
     return response()->json(['message' => "hello laravel app"]);
 });
 
 Route::post('/test-post', function (Request $request) {
+    Log::info('🔔 POST /test-post hit', ['data' => $request->all()]);
+
     return response()->json([
         'success' => true,
         'message' => 'POST request received!',
